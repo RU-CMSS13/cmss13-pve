59730d34c7f75871baec285c299515ae7edcb319
Taushai <staszhex@gmail.com>
2026-09-24 14:42:01 +0300

    body upd

--- tgui/packages/tgui/interfaces/BodyPicker.tsx
+++ tgui/packages/tgui/interfaces/BodyPicker.tsx
@@ -1,15 +1,5 @@
-import { useState } from 'react';
-
 import { useBackend } from '../backend';
-import {
-  Box,
-  Button,
-  ColorBox,
-  DmIcon,
-  Modal,
-  Stack,
-  Tooltip,
-} from '../components';
+import { Button, ColorBox, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
 type PickerData = {
@@ -24,89 +14,21 @@ type PickerData = {
 };
 
 export const BodyPicker = () => {
-  const { data } = useBackend<PickerData>();
-
-  const { icon, body_size, body_type, skin_color, body_types, body_sizes } =
-    data;
-
-  const [picker, setPicker] = useState<'type' | 'size' | undefined>();
-
-  const unselectedBodyType = body_types.filter(
-    (val) => val.icon !== body_type,
-  )[0];
-
-  const unselectedBodySize = body_sizes.filter(
-    (val) => val.icon !== body_size,
-  )[0];
-
   return (
-    <Window width={390} height={180} theme={'crtblue'}>
+    <Window width={420} height={320} theme={'crtblue'}>
       <Window.Content className="BodyPicker">
-        {picker && (
-          <Modal m={1}>
-            <TypePicker picker={setPicker} toUse={picker} />
-          </Modal>
-        )}
-        <Stack>
-          <Stack.Item>
-            <Stack vertical>
+        <Stack fill>
+          <Stack.Item grow>
+            <Stack vertical fill>
               <Stack.Item>
-                <DmIcon
-                  icon={icon}
-                  icon_state={`${skin_color}_torso_${body_size}_${body_type}`}
-                  width={'128px'}
-                  mt={-5}
-                />
+                <TypePicker toUse="type" />
               </Stack.Item>
-              <Stack width="100%" fill mt={-4} justify="space-around">
-                <Stack.Item>
-                  <Button width={'4em'} height={'4em'}>
-                    <Tooltip
-                      content={'Change Body Type'}
-                      position="bottom-start"
-                    >
-                      <Box
-                        position="relative"
-                        onClick={() => setPicker('type')}
-                      >
-                        <DmIcon
-                          position="relative"
-                          icon={icon}
-                          icon_state={`${skin_color}_torso_${body_size}_${unselectedBodyType.icon}`}
-                          width={'80px'}
-                          right={'22px'}
-                          bottom={'18px'}
-                        />
-                      </Box>
-                    </Tooltip>
-                  </Button>
-                </Stack.Item>
-                <Stack.Item>
-                  <Button width={'4em'} height={'4em'}>
-                    <Tooltip
-                      content={'Change Body Size'}
-                      position="bottom-start"
-                    >
-                      <Box
-                        position="relative"
-                        onClick={() => setPicker('size')}
-                      >
-                        <DmIcon
-                          position="relative"
-                          icon={icon}
-                          icon_state={`${skin_color}_torso_${unselectedBodySize.icon}_${body_type}`}
-                          width={'80px'}
-                          right={'22px'}
-                          bottom={'18px'}
-                        />
-                      </Box>
-                    </Tooltip>
-                  </Button>
-                </Stack.Item>
-              </Stack>
+              <Stack.Item>
+                <TypePicker toUse="size" />
+              </Stack.Item>
             </Stack>
           </Stack.Item>
-          <Stack.Item>
+          <Stack.Item grow>
             <ColorOptions />
           </Stack.Item>
         </Stack>
@@ -115,48 +37,30 @@ export const BodyPicker = () => {
   );
 };
 
-const TypePicker = (props: {
-  readonly picker: (_) => void;
-  readonly toUse: 'type' | 'size';
-}) => {
+const TypePicker = (props: { readonly toUse: 'type' | 'size' }) => {
   const { data, act } = useBackend<PickerData>();
 
-  const { picker, toUse } = props;
+  const { toUse } = props;
 
-  const { body_type, body_types, skin_color, body_size, body_sizes, icon } =
-    data;
+  const { body_type, body_types, body_size, body_sizes } = data;
 
   const toIterate = toUse === 'type' ? body_types : body_sizes;
 
   const active = toUse === 'type' ? body_type : body_size;
 
   return (
-    <Stack>
+    <Section title={toUse === 'type' ? 'Body Type' : 'Body Size'}>
       {toIterate.map((type) => (
-        <Stack.Item key={type.name}>
-          <Tooltip content={type.name}>
-            <Box
-              onClick={() => {
-                picker(undefined);
-                act(toUse, { name: type.name });
-              }}
-              position="relative"
-              className={`typePicker ${active === type.icon ? 'active' : ''}`}
-            >
-              <DmIcon
-                icon={icon}
-                icon_state={
-                  toUse === 'type'
-                    ? `${skin_color}_torso_${body_size}_${type.icon}`
-                    : `${skin_color}_torso_${type.icon}_${body_type}`
-                }
-                width={'80px'}
-              />
-            </Box>
-          </Tooltip>
-        </Stack.Item>
+        <Button
+          key={type.name}
+          fluid
+          selected={active === type.icon}
+          onClick={() => act(toUse, { name: type.name })}
+        >
+          {type.name}
+        </Button>
       ))}
-    </Stack>
+    </Section>
   );
 };
 
@@ -166,18 +70,18 @@ const ColorOptions = () => {
   const { skin_color, skin_colors } = data;
 
   return (
-    <Stack wrap={'wrap'} width={'250px'}>
+    <Section title="Skin Color" fill scrollable>
       {skin_colors.map((color) => (
-        <Stack.Item key={color.name} className="colorPickerContainer">
-          <ColorBox
-            color={color.color}
-            p={3.5}
-            mt={0.5}
-            onClick={() => act('color', { name: color.name })}
-            className={`colorPicker ${skin_color === color.icon ? 'active' : ''}`}
-          />
-        </Stack.Item>
+        <Button
+          key={color.name}
+          fluid
+          selected={skin_color === color.icon}
+          onClick={() => act('color', { name: color.name })}
+        >
+          <ColorBox color={color.color} mr={1} />
+          {color.name}
+        </Button>
       ))}
-    </Stack>
+    </Section>
   );
 };
